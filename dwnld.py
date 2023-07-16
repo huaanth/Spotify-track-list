49e171f6ab3c5f9736b30deb66c6cbd5a159d17e
huaanth <115844975+huaanth@users.noreply.github.com>
2023-07-16 16:26:49 -0400

    Add files via upload
    I'm thinking of making a project to scrape youtube to create a music playlist with all the users' saved spotify songs

---
+++ dwnld.py
@@ -0,0 +1,21 @@
+from bs4 import BeautifulSoup
+from requests_html import HTMLSession
+from pathlib import Path
+import requests
+import youtube_dl
+import pandas
+import os
+
+def ScrapeID(query):
+    print("Getting video: ", query)
+    basic = "http://www.youtube.com/results?search_query=" #it will just put the song name after the equal sign and find it on youtube
+    url = (basic + query)
+    url.replace(" ","+")
+    page = requests.get(url)
+    session = HTMLSession()
+    response = session.get(url)
+    response.html.render(sleep=1)
+    soup = BeautifulSoup(response.html.html,"html.parser")
+
+    results = soup.find("a", id = "video-title")
+    return results["href"].split('/watch?v=')[1]
